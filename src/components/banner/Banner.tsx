4e570c553ae0eb86369985f71fd37a34b837ecac
ahmadjonov <ahmadjonovasilbek624@gmail.com>
2025-04-07 19:30:41 +0500

    banner tugadi

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -1,17 +1,20 @@
 
 import Btn from "../../ui/Button"
+import bannerImg from "../../assets/images/banner.png";
 
 function Banner() {
   return (
     <div className="bannerComponent w-full">
       <div className="container max-w-6xl mx-auto my-0">
-        <div className="banner w-full rounded-[20px] pt-[80px] pl-[100px] pb-[86px] bg-[#EDEEF2]">
+        <div className="banner w-full rounded-[20px] pt-[80px] pl-[100px] pb-[82px] bg-[#EDEEF2]">
           <h1 className="text-[40px] uppercase font-bold ">
           Katta chegirma
           </h1>
-          <p className="text-base font-light mb-10 w-full max-w-[350px]">MDF eshiklarini 40% chegirma narxda sotib 
+          <p className="text-base font-light mb-10 w-full max-w-[350px] text-[#454545]">MDF eshiklarini 40% chegirma narxda sotib 
           olishga ulgurib qoling</p>
           <Btn />
+          <img src={bannerImg} alt="banner" className="bannerImg absolute top-0 right-[10px]" />
+          <h3 className="bannerRek uppercase bg-[#FF2020] py-[10px] px-[20px] absolute top-0 left-0 text-white text-base font-bold ">chegirma</h3>
         </div>
       </div>
     </div>
--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -1,8 +1,11 @@
 
-
+import "../App.css"
 function Btn() {
   return (
-    <button className=" text-base font-light bg-blackrounded-lgbg-black text-white py-2 px-4 rounded-2xl hover:bg-white hover:text-black transition duration-300">Sotib olish</button>
+    <button className="bg-black text-white py-2 px-4 rounded-2xl hover:bg-white hover:text-black transition duration-300">
+    Sotib olish
+  </button>
+  
   )
 }
 
